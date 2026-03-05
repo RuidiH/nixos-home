245be5403ebe887431d7ddb3659891391aadecc0
RuidiH <764342051@qq.com>
2026-03-05 12:32:48 -0800

    simplify starship

--- home/programs/starship.nix
+++ home/programs/starship.nix
@@ -4,7 +4,23 @@
     enable = true;
     settings = {
       add_newline = false;
-      line_break.disabled = true;
+
+      format = "$directory$git_branch$python$nix_shell$aws$line_break$character";
+
+      directory = {
+        truncation_length = 1;
+        truncate_to_repo = true;
+      };
+
+      git_branch.format = " [$branch]($style) ";
+
+      python.format = "[🐍](yellow) ";
+
+      nix_shell.format = "[❄️](blue) ";
+
+      aws.format = "[☁️](yellow) ";
+
+      cmd_duration.disabled = true;
     };
   };
 }
